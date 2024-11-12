56d12c48a056508e66bc1ea1e2da8efe9bcb3303
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2024-11-12 16:51:19 -0300

    update package

--- POO_CSharp/Aula11.1/obj/Debug/net6.0-windows/Aula11.1.AssemblyInfo.cs
+++ POO_CSharp/Aula11.1/obj/Debug/net6.0-windows/Aula11.1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula11.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c33710e3de477e874685816a361f87b951e29be3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+443b9e827a136bbaae582c8dedefc57035613d65")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula11.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula11.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
